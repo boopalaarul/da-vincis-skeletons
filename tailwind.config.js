6d6056b2e07d5bd437993244859ccdc2d9d06fc2
boopalaarul <boopalaarul@yahoo.com>
2023-12-21 02:08:44 -0500

    first commit, tailwind still only takes input from the pug files, figure out how to point it only to the JS I create (don't have it waste time scanning the node modules' JS files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	
+/** paths to all files used as "templates" for generated CSS:
+ * in other words, paths to all html and js files in the project */
+  content: ["./**/*.pug"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
